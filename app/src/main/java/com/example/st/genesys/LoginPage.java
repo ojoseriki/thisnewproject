b13fbd2a2e9664686089363f6ad359ffc20142d4
mkoveni <mkoveni@gmail.com>
2018-05-15 00:35:05 +0200

    Improved colors and controls on LoginPage,Employee Signup, Company Signup, Goods Page

--- app/src/main/java/com/example/st/genesys/LoginPage.java
+++ app/src/main/java/com/example/st/genesys/LoginPage.java
@@ -1,8 +1,8 @@
 package com.example.st.genesys;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,26 +13,38 @@ public class LoginPage extends AppCompatActivity  {
     Button log ,regis;
    private EditText user,pass;
 
+   private TextView lblIndividual, lblCompany;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
-        regis = (Button) findViewById(R.id.register_btn);
-        user = (EditText) findViewById(R.id.user_edit);
-        pass = (EditText) findViewById(R.id.passowrd_edit);
-        log = (Button) findViewById(R.id.login_btn);
+        user = findViewById(R.id.txtUsername);
+        pass = findViewById(R.id.txtPassword);
+        log =  findViewById(R.id.btnLogin);
 
-     regis.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-         Intent intent = new Intent(LoginPage.this,DecidePage.class);
-         startActivity(intent);
+        lblIndividual = findViewById(R.id.lblCreateAccount);
+        lblCompany = findViewById(R.id.lblCreateCompany);
 
-         }
-     });
+        lblIndividual.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View view) {
 
+                startActivity(new Intent(LoginPage.this, employeeSignup.class));
+            }
+        });
+
+        lblCompany.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(LoginPage.this, companySignup.class));
+            }
+        });
 
 log.setOnClickListener(new View.OnClickListener() {
     @Override
--- app/src/main/java/com/example/st/genesys/employeeSignup.java
+++ app/src/main/java/com/example/st/genesys/employeeSignup.java
@@ -1,18 +1,16 @@
 package com.example.st.genesys;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.example.st.genesys.dataLayer.DatabaseHelper;
-import com.example.st.genesys.dataLayer.entities.Employee;
 
 public class employeeSignup extends AppCompatActivity {
 DatabaseHelper mydb;
- EditText fname,lname,addres,jobdes,emplypassword,Employcode;
+ EditText fname,lname,addres,jobdes,emplypassword;
  Button Employsubmit_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +23,6 @@ DatabaseHelper mydb;
         addres = (EditText) findViewById(R.id.employaddres);
         jobdes = (EditText) findViewById(R.id.employjobdescription);
         emplypassword = (EditText)findViewById(R.id.employpass);
-        Employcode = (EditText) findViewById(R.id.employcode);
         Employsubmit_btn = (Button) findViewById(R.id.employsignup_btn);
         addDataEmploy();
 
@@ -35,16 +32,7 @@ DatabaseHelper mydb;
       Employsubmit_btn.setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View v) {
-              Employee employee = new Employee(fname.getText().toString(), lname.getText().toString(),
-                      addres.getText().toString(), jobdes.getText().toString(), emplypassword.getText().toString(),
-                      Integer.parseInt(Employcode.getText().toString()));
-
-              long result = Employee.create(employeeSignup.this,employee);
-
-              if (result != -1){
-                  Toast.makeText(employeeSignup.this,"Sign up is successful",Toast.LENGTH_LONG ).show();
-              }else
-                  Toast.makeText(employeeSignup.this,"sign UP NOT successful", Toast.LENGTH_LONG).show();
+              //Add employee to Api Resource
           }
       });
 
